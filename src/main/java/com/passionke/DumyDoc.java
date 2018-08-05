7cc761150893b1aa6a95c6a72ca5e9087d560b5c
刀枫 <jiqing.kejq@antfin.com>
2018-08-06 07:49:52 +0800

    update pom

---
+++ src/main/java/com/passionke/DumyDoc.java
@@ -0,0 +1,15 @@
+/**
+ * Alipay.com Inc. Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.passionke;
+
+/**
+ *
+ * @author daofeng
+ * @version $Id: DumyDoc.java, v 0.1 2018年08月06日 上午3:29 daofeng Exp $
+ *
+ * just for generate javadoc.jar for
+ * https://oss.sonatype.org/#stagingRepositories java doc fucking check
+ */
+public class DumyDoc {
+}
---
+++ src/main/java/com/passionke/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @author daofeng
+ */
+package com.passionke;
